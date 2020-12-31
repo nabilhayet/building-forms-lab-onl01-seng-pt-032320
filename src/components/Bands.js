ef6b18d63c0a5ab7bda7be52da7ba661057f1053
nabilhayet <nabil4457@yahoo.com>
2020-12-30 20:34:00 -0500

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Bands = props => {
+    debugger
   const bands = props.bands.map((band, index) => {
     return <li key={index}>{band.name}</li>;
   });
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,9 +1,11 @@
 export default function manageBand(state = {
+ 
   bands: [],
 }, action) {
+  console.log(action)
   switch (action.type) {
     case 'ADD_BAND':
-      return { ...state, bands: [...state.bands, action.band] }
+      return { ...state, bands: [...state.bands, action.payload] }
     default:
       return state;
   }
